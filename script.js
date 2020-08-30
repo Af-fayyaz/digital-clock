71b766d89c3136c853ae24eb91000039ef5fccaa
Af Fayyaz <61106289+Af-fayyaz@users.noreply.github.com>
2020-08-30 17:08:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -40,22 +40,3 @@ function showTime(){
   setTimeout(showTime, 1000);
 
 }
-
-
-// function hChange(){
-//
-//   var date = new Date();
-//   var h = date.getHours();       //0-23
-//   var m = date.getMinutes();     //0-59
-//   var s = date.getSeconds();     //0-59
-//
-//   h = (h < 10)? "0" +h : h;
-//   m = (m < 10)? "0" +m : m;
-//   s = (s < 10)? "0" +s : s;
-//
-//   var timef = h +":" +m +":" +s +" ";
-//
-//   document.getElementById("digitalclock").innerHTML = timef;
-//
-//   setTimeout(hChange, 1000);
-// }
